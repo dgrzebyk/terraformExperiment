f44545e5a323bbfa29911c47abe29f48c64bbdda
dgrzebyk <daniel.grzebyk@sappi.com>
2024-08-13 16:48:39 +0200

    ENH: zipping cloud functions with Terraform

--- tf_dataform_proj/modules/allocation_upload/cloudfunctions.tf
+++ tf_dataform_proj/modules/allocation_upload/cloudfunctions.tf
@@ -1,3 +1,19 @@
+# TODO: Keep zipping in a loop
+
+data "archive_file" "ratios_to_bq_zip" {
+  type        = "zip"
+  output_path = "cloud_functions/allocation_ratios.zip"
+  source_dir  = "../cloud_functions/allocation_ratios"
+  provider    = archive
+}
+
+# data "archive_file" "snps_a005" {
+#   type        = "zip"
+#   output_path = "cloud_functions/snps_a005.zip"
+#   source_dir  = "../../../cloud_functions/snps_a005"
+#   provider    = archive
+# }
+
 # Deploys cloud function uploading allocation ratios to BigQuery
 resource "google_cloudfunctions2_function" "ratios_to_bq" {
   name = "ratios_to_bq"
@@ -37,43 +53,43 @@ resource "google_cloudfunctions2_function" "ratios_to_bq" {
   }
 }
 
-resource "google_cloudfunctions2_function" "snp_a005_to_bq" {
-  name = "snp_a005_to_bq"
-  location = local.region
-  description = "Uploading data from BISS BOA SNP_A005 report to BigQuery"
-
-  build_config {
-    runtime = "python311"
-    entry_point = "save_snp_a005_to_bq"  # Set the entry point
-    source {
-      storage_source {
-        bucket = google_storage_bucket.cf_allocation_upload.name
-        object = google_storage_bucket_object.cloud_function_snp_a005.name
-      }
-    }
-  }
-
-  service_config {
-    max_instance_count  = 1
-    available_memory    = "512M"
-    timeout_seconds     = 60
-    service_account_email = google_service_account.cloud_functions.email
-  }
-
-  event_trigger {
-    event_type = "google.cloud.storage.object.v1.finalized"
-    retry_policy = "RETRY_POLICY_RETRY"
-    service_account_email = google_service_account.cloud_storage.email
-    event_filters {
-      attribute = "bucket"
-      value = google_storage_bucket.allocation_upload.name
-    }
-#     trigger_region = local.region
-#     event_type = "google.cloud.pubsub.topic.v1.messagePublished"
-#     pubsub_topic = google_pubsub_topic.snp_a005_upload.id
+# resource "google_cloudfunctions2_function" "snp_a005_to_bq" {
+#   name = "snp_a005_to_bq"
+#   location = local.region
+#   description = "Uploading data from BISS BOA SNP_A005 report to BigQuery"
+#
+#   build_config {
+#     runtime = "python311"
+#     entry_point = "save_snp_a005_to_bq"  # Set the entry point
+#     source {
+#       storage_source {
+#         bucket = google_storage_bucket.cf_allocation_upload.name
+#         object = google_storage_bucket_object.cloud_function_snp_a005.name
+#       }
+#     }
+#   }
+#
+#   service_config {
+#     max_instance_count  = 1
+#     available_memory    = "512M"
+#     timeout_seconds     = 60
+#     service_account_email = google_service_account.cloud_functions.email
+#   }
+#
+#   event_trigger {
+#     event_type = "google.cloud.storage.object.v1.finalized"
 #     retry_policy = "RETRY_POLICY_RETRY"
-  }
-}
+#     service_account_email = google_service_account.cloud_storage.email
+#     event_filters {
+#       attribute = "bucket"
+#       value = google_storage_bucket.allocation_upload.name
+#     }
+# #     trigger_region = local.region
+# #     event_type = "google.cloud.pubsub.topic.v1.messagePublished"
+# #     pubsub_topic = google_pubsub_topic.snp_a005_upload.id
+# #     retry_policy = "RETRY_POLICY_RETRY"
+#   }
+# }
 
 # This cloud function is triggered manually by the user
 # resource "google_cloudfunctions2_function" "bq_to_txt" {
