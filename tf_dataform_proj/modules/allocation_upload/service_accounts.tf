4cd52ddeeb8c3fa4d99b8ca23a3f1616cc3f6046
dgrzebyk <daniel.grzebyk@sappi.com>
2024-08-13 16:05:54 +0200

    ADD: cloud functions service account and permissions

--- tf_dataform_proj/modules/allocation_upload/service_accounts.tf
+++ tf_dataform_proj/modules/allocation_upload/service_accounts.tf
@@ -13,8 +13,7 @@ resource "google_service_account" "cloud_storage" {
   display_name = "Cloud Storage Service Account"
 }
 
-// Enable notifications by giving the correct IAM permission to the unique service account.
-
+# Allow buckets to send notifications to Pub/Sub topics
 data "google_storage_project_service_account" "gcs_account" {
 }
 
@@ -22,4 +21,20 @@ resource "google_pubsub_topic_iam_binding" "binding" {
   topic   = google_pubsub_topic.ratios_upload.id
   role    = "roles/pubsub.publisher"
   members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
+}
+
+resource "google_cloudfunctions2_function_iam_member" "invoker" {
+  project        = google_cloudfunctions2_function.ratios_to_bq.project
+  location       = google_cloudfunctions2_function.ratios_to_bq.location
+  cloud_function = google_cloudfunctions2_function.ratios_to_bq.name
+  role           = "roles/cloudfunctions.invoker"
+  member         = "serviceAccount:${google_service_account.cloud_functions.email}"
+}
+
+resource "google_cloud_run_service_iam_member" "cloud_run_invoker" {
+  project  = google_cloudfunctions2_function.snp_a005_to_bq.project
+  location = google_cloudfunctions2_function.snp_a005_to_bq.location
+  service  = google_cloudfunctions2_function.snp_a005_to_bq.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.cloud_storage.email}"
 }
