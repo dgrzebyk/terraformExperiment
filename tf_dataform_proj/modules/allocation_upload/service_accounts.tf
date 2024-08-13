de50443a9d2d5182567e49d3f73c74c37954c7a8
dgrzebyk <daniel.grzebyk@sappi.com>
2024-08-13 14:40:51 +0200

    ENH: GCS & Cloud Functions service accounts

--- tf_dataform_proj/modules/allocation_upload/service_accounts.tf
+++ tf_dataform_proj/modules/allocation_upload/service_accounts.tf
@@ -4,6 +4,22 @@ resource "google_service_account" "dataform" {
 }
 
 resource "google_service_account" "cloud_functions" {
-  account_id   = "cloud-function-sa"
-  display_name = "Dataform Service Account"
+  account_id   = "cloud-functions-sa"
+  display_name = "Cloud Functions Service Account"
+}
+
+resource "google_service_account" "cloud_storage" {
+  account_id   = "gcs-sa"
+  display_name = "Cloud Storage Service Account"
+}
+
+// Enable notifications by giving the correct IAM permission to the unique service account.
+
+data "google_storage_project_service_account" "gcs_account" {
+}
+
+resource "google_pubsub_topic_iam_binding" "binding" {
+  topic   = google_pubsub_topic.ratios_upload.id
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
 }
