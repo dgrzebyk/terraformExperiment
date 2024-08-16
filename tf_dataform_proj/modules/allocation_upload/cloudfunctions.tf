3de121038dec9d32482fa6dd4d81ef98268df2a9
Daniel Grzebyk <daniel.grzebyk@sappi.com>
2024-08-16 14:44:00 +0200

    ENH: impersonating service accounts, granting permissions

--- tf_dataform_proj/modules/allocation_upload/cloudfunctions.tf
+++ tf_dataform_proj/modules/allocation_upload/cloudfunctions.tf
@@ -1,18 +1,14 @@
-# TODO: Keep zipping in a loop
+data "archive_file" "functions_zips" {
+  for_each = toset([
+    "allocation_ratios",
+    "bq_to_txt",
+    "snp_a005"
+  ])
 
-data "archive_file" "ratios_to_bq_zip" {
   type        = "zip"
-  output_path = "cloud_functions/allocation_ratios.zip"
-  source_dir  = "../cloud_functions/allocation_ratios"
-  provider    = archive
+  output_path = "../cloud_functions/${each.key}.zip"
+  source_dir  = "../cloud_functions/${each.key}"
 }
-
-# data "archive_file" "snps_a005" {
-#   type        = "zip"
-#   output_path = "cloud_functions/snps_a005.zip"
-#   source_dir  = "../../../cloud_functions/snps_a005"
-#   provider    = archive
-# }
 
 # Deploys cloud function uploading allocation ratios to BigQuery
 resource "google_cloudfunctions2_function" "ratios_to_bq" {
@@ -23,10 +19,11 @@ resource "google_cloudfunctions2_function" "ratios_to_bq" {
   build_config {
     runtime = "python311"
     entry_point = "allocation_ratios"  # Set the entry point
+    service_account = "projects/${var.project}/serviceAccounts/${google_service_account.cloud_functions.email}"
     source {
       storage_source {
         bucket = google_storage_bucket.cf_allocation_upload.name
-        object = google_storage_bucket_object.cloud_function_ratios.name
+        object = google_storage_bucket_object.zip_files["allocation_ratios.zip"].name
       }
     }
   }
@@ -36,6 +33,11 @@ resource "google_cloudfunctions2_function" "ratios_to_bq" {
     available_memory    = "512M"
     timeout_seconds     = 60
     service_account_email = google_service_account.cloud_functions.email
+    environment_variables = {
+      GOOGLE_CLOUD_PROJECT_ID = var.project
+      BQ_DATASET = google_bigquery_dataset.allocation.dataset_id
+      BQ_TABLE = google_bigquery_table.ratios_temp.table_id
+    }
   }
 
   event_trigger {
@@ -44,66 +46,63 @@ resource "google_cloudfunctions2_function" "ratios_to_bq" {
     service_account_email = google_service_account.cloud_storage.email
     event_filters {
       attribute = "bucket"
-      value = google_storage_bucket.allocation_upload.name
+      value = google_storage_bucket.allocation_excels.name
     }
-#     trigger_region = local.region
-#     event_type = "google.cloud.pubsub.topic.v1.messagePublished"
-#     pubsub_topic = google_pubsub_topic.ratios_upload.id
-#     retry_policy = "RETRY_POLICY_RETRY"
   }
 }
 
-# resource "google_cloudfunctions2_function" "snp_a005_to_bq" {
-#   name = "snp_a005_to_bq"
-#   location = local.region
-#   description = "Uploading data from BISS BOA SNP_A005 report to BigQuery"
-#
-#   build_config {
-#     runtime = "python311"
-#     entry_point = "save_snp_a005_to_bq"  # Set the entry point
-#     source {
-#       storage_source {
-#         bucket = google_storage_bucket.cf_allocation_upload.name
-#         object = google_storage_bucket_object.cloud_function_snp_a005.name
-#       }
-#     }
-#   }
-#
-#   service_config {
-#     max_instance_count  = 1
-#     available_memory    = "512M"
-#     timeout_seconds     = 60
-#     service_account_email = google_service_account.cloud_functions.email
-#   }
-#
-#   event_trigger {
-#     event_type = "google.cloud.storage.object.v1.finalized"
-#     retry_policy = "RETRY_POLICY_RETRY"
-#     service_account_email = google_service_account.cloud_storage.email
-#     event_filters {
-#       attribute = "bucket"
-#       value = google_storage_bucket.allocation_upload.name
-#     }
-# #     trigger_region = local.region
-# #     event_type = "google.cloud.pubsub.topic.v1.messagePublished"
-# #     pubsub_topic = google_pubsub_topic.snp_a005_upload.id
-# #     retry_policy = "RETRY_POLICY_RETRY"
-#   }
-# }
+resource "google_cloudfunctions2_function" "snp_a005_to_bq" {
+  name = "snp_a005_to_bq"
+  location = local.region
+  description = "Copying data from SNP_A005 BISS BOA .xlsx reports to BigQuery"
 
-# This cloud function is triggered manually by the user
+  build_config {
+    runtime = "python311"
+    entry_point = "save_snp_a005_to_bq"
+    service_account = "projects/${var.project}/serviceAccounts/${google_service_account.cloud_functions.email}"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.cf_allocation_upload.name
+        object = google_storage_bucket_object.zip_files["snp_a005.zip"].name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count  = 1
+    available_memory    = "512M"
+    timeout_seconds     = 60
+    service_account_email = google_service_account.cloud_functions.email
+    environment_variables = {
+      GOOGLE_CLOUD_PROJECT_ID = var.project
+      BQ_DATASET = google_bigquery_dataset.allocation.dataset_id
+      BQ_TABLE = google_bigquery_table.SNP_A005.table_id
+    }
+  }
+
+  event_trigger {
+    event_type = "google.cloud.storage.object.v1.finalized"
+    retry_policy = "RETRY_POLICY_RETRY"
+    service_account_email = google_service_account.cloud_storage.email
+    event_filters {
+      attribute = "bucket"
+      value = google_storage_bucket.snp_a005_reports.name
+    }
+  }
+}
+
 # resource "google_cloudfunctions2_function" "bq_to_txt" {
 #   name = "bq_to_txt"
 #   location = local.region
-#   description = "Transferring allocations from BigQuery to .txt files in GCS"
+#   description = "Copying Dataform output from BigQuery to SAP-compatible .txt files"
 #
 #   build_config {
 #     runtime = "python311"
-#     entry_point = "bq_to_txt"  # Set the entry point
+#     entry_point = "bq_to_txt"
 #     source {
 #       storage_source {
 #         bucket = google_storage_bucket.cf_allocation_upload.name
-#         object = google_storage_bucket_object.cloud_function_bq_to_txt.name
+#         object = google_storage_bucket_object.zip_files["bq_to_txt.zip"].name
 #       }
 #     }
 #   }
@@ -112,13 +111,16 @@ resource "google_cloudfunctions2_function" "ratios_to_bq" {
 #     max_instance_count  = 1
 #     available_memory    = "256M"
 #     timeout_seconds     = 60
-#     service_account_email = google_service_account.cloud_storage.email
+#     service_account_email = google_service_account.cloud_functions.email
 #   }
-
+#
 #   event_trigger {
-#     trigger_region = local.region
-#     event_type = "google.cloud.pubsub.topic.v1.messagePublished"
-#     pubsub_topic = google_pubsub_topic.snp_a005_upload.id
+#     event_type = "google.cloud.storage.object.v1.finalized"
 #     retry_policy = "RETRY_POLICY_RETRY"
+# #     service_account_email = "${data.google_storage_project_service_account.gcs_account.email_address}"
+#     event_filters {
+#       attribute = "bucket"
+#       value = google_storage_bucket.allocation_upload.name
+#     }
 #   }
 # }
--- tf_dataform_proj/modules/allocation_upload/service_accounts.tf
+++ tf_dataform_proj/modules/allocation_upload/service_accounts.tf
@@ -18,25 +18,68 @@ resource "google_service_account" "cloud_storage" {
   display_name = "Cloud Storage Service Account"
 }
 
-### GRANT CLOUD FUNCTIONS PERMISSIONS ###
-resource "google_cloudfunctions2_function_iam_member" "cf_invoker" {
-  project        = google_cloudfunctions2_function.ratios_to_bq.project
-  location       = google_cloudfunctions2_function.ratios_to_bq.location
-  cloud_function = google_cloudfunctions2_function.ratios_to_bq.name
-  role           = "roles/cloudfunctions.invoker"
-  member         = "serviceAccount:${google_service_account.cloud_functions.email}"
+data "google_storage_project_service_account" "gcs_account" {
 }
 
-resource "google_cloudfunctions2_function_iam_member" "run_invoker" {
-  project        = google_cloudfunctions2_function.ratios_to_bq.project
-  location       = google_cloudfunctions2_function.ratios_to_bq.location
-  cloud_function = google_cloudfunctions2_function.ratios_to_bq.name
-  role           = "roles/run.invoker"
-  member         = "serviceAccount:${google_service_account.cloud_functions.email}"
+# Grant permissions to GCS SA to receive Eventarc events
+resource "google_project_iam_member" "gcs_sa_roles" {
+  for_each = toset([
+    "roles/eventarc.eventReceiver",
+    "roles/run.invoker",
+    "roles/pubsub.publisher"
+  ])
+
+  project = var.project
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.cloud_storage.email}"
+
+  depends_on = [
+    google_service_account.cloud_storage
+  ]
 }
 
-### GRANT PERMISSIONS TO THE DEFAULT DATAFORM SERVICE ACCOUNT ###
-# Reading secrets is necessary for Dataform to access GitHub Token
+resource "google_project_iam_member" "cloud_function_roles" {
+  for_each = toset([
+    "roles/cloudfunctions.admin",
+    "roles/cloudfunctions.invoker",
+    "roles/run.invoker",
+    "roles/cloudbuild.builds.builder",
+    "roles/storage.objectAdmin",
+    "roles/logging.logWriter",
+    "roles/bigquery.jobUser",
+    "roles/bigquery.dataEditor"
+  ])
+
+  project = var.project
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.cloud_functions.email}"
+
+  depends_on = [
+    google_service_account.cloud_functions
+  ]
+}
+
+# Dataform does not have this handle yet which is why we had to rely on naming convention
+data "google_compute_default_service_account" "default" {
+}
+
+resource "google_service_account_iam_member" "cf_sa_impersonation" {
+  # User-defined service account to be impersonated
+  service_account_id = "projects/${var.project}/serviceAccounts/${google_service_account.cloud_functions.email}"
+  role = "roles/iam.serviceAccountTokenCreator"
+  # Member is the primary identity that can impersonate user-defined service account
+  member = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+}
+
+### GRANT DATAFORM PERMISSIONS ###
+# Allow default dataform SA to impersonate user-defined dataform SA
+resource "google_service_account_iam_member" "custom_service_account_token_creator" {
+  service_account_id = "projects/${var.project}/serviceAccounts/${google_service_account.dataform.email}"
+  role = "roles/iam.serviceAccountTokenCreator"
+  member = "serviceAccount:service-${data.google_project.my_project.number}@gcp-sa-dataform.iam.gserviceaccount.com"
+}
+
+# Allow default Dataform SA to access GitHub Token - TODO: Why not user-defined SA?
 resource "google_secret_manager_secret_iam_member" "access" {
   project   = var.project
   secret_id = google_secret_manager_secret.secret.id
@@ -44,32 +87,6 @@ resource "google_secret_manager_secret_iam_member" "access" {
   member    = "serviceAccount:service-${data.google_project.my_project.number}@gcp-sa-dataform.iam.gserviceaccount.com"
 }
 
-### IMPERSONATE DEFAULT DATAFORM SERVICE ACCOUNT ###
-resource "google_service_account_iam_binding" "custom_service_account_token_creator" {
-  service_account_id = "projects/${var.project}/serviceAccounts/${google_service_account.dataform.email}"
-
-  role = "roles/iam.serviceAccountTokenCreator"
-
-  members = [
-    "serviceAccount:service-${data.google_project.my_project.number}@gcp-sa-dataform.iam.gserviceaccount.com"
-  ]
-}
-
-resource "google_secret_manager_secret_iam_binding" "github_secret_accessor" {
-  secret_id = google_secret_manager_secret.secret.secret_id
-
-  role = "roles/secretmanager.secretAccessor"
-
-  members = [
-    "serviceAccount:service-${data.google_project.my_project.number}@gcp-sa-dataform.iam.gserviceaccount.com"
-  ]
-
-  depends_on = [
-    google_secret_manager_secret.secret
-  ]
-}
-
-### GRANT CUSTOM DATAFORM SERVICE ACCOUNT THE NECESSARY PERMISSIONS ###
 resource "google_project_iam_member" "dataform_roles" {
   for_each = toset([
     "roles/bigquery.dataEditor",
