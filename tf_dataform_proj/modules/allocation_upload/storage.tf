cc6334160695b6690fc38a1b1a95b7d7dc709b1f
Daniel Grzebyk <daniel.grzebyk@sappi.com>
2024-08-16 14:41:48 +0200

    FIX: separate storage buckets for compatibility with cloud function triggers

--- tf_dataform_proj/modules/allocation_upload/storage.tf
+++ tf_dataform_proj/modules/allocation_upload/storage.tf
@@ -1,6 +1,15 @@
-# Defining bucket containing data
-resource "google_storage_bucket" "allocation_upload" {
-  name     = "allocation_upload"
+resource "google_storage_bucket" "allocation_excels" {
+  name     = "allocation_excels"
+  location = local.region
+}
+
+resource "google_storage_bucket" "snp_a005_reports" {
+  name     = "snp_a005_reports"
+  location = local.region
+}
+
+resource "google_storage_bucket" "sap_txts" {
+  name     = "sap_txts"
   location = local.region
 }
 
@@ -10,30 +19,16 @@ resource "google_storage_bucket" "cf_allocation_upload" {
   location = local.region
 }
 
-# Creating 3 folders in the data bucket
-resource "google_storage_bucket_object" "snp_a005_reports" {
-  name   = "snp_a005_reports/" # folder name should end with '/'
-  content = " "            # content is ignored but should be non-empty
-  bucket = google_storage_bucket.allocation_upload.name
-}
+# Storing cloud function code in a GCS bucket
+resource "google_storage_bucket_object" "zip_files" {
+  for_each = {
+    "allocation_ratios.zip" = "../cloud_functions/allocation_ratios.zip",
+    "bq_to_txt.zip"         = "../cloud_functions/bq_to_txt.zip",
+    "snp_a005.zip"          = "../cloud_functions/snp_a005.zip"
+  }
 
-resource "google_storage_bucket_object" "ratios" {
-  name   = "ratios/" # folder name should end with '/'
-  content = " "            # content is ignored but should be non-empty
-  bucket = google_storage_bucket.allocation_upload.name
-}
-
-resource "google_storage_bucket_object" "txts" {
-  name   = "txts/" # folder name should end with '/'
-  content = " "            # content is ignored but should be non-empty
-  bucket = google_storage_bucket.allocation_upload.name
-}
-
-### CLOUD FUNCTIONS CODE ###
-# Stores cloud function code in a GCS bucket
-resource "google_storage_bucket_object" "cloud_function_ratios" {
-  name   = "allocation_ratios.zip"
+  name   = each.key            # the file name
   bucket = google_storage_bucket.cf_allocation_upload.name
-  # source - path to zip file containing the code from this repository root
-  source = "cloud_functions/allocation_ratios.zip"
+  source = each.value          # the file path
 }
+
